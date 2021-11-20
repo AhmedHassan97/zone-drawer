6a119cf4c12d456774ddaf328de4cfcb4b455e03
AhmedHassan97 <ahahmed202025@gmail.com>
2021-11-21 01:20:15 +0200

    feat:add map page

---
+++ pages/map.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+import MapComponent from "../app/components/map";
+const Home: NextPage = () => {
+  return (
+    <div>
+      <MapComponent />
+    </div>
+  );
+};
+
+export default Home;
