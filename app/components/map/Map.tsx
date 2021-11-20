bb19eff5bb31da158a6884e30e6f54cf0744e4d2
AhmedHassan97 <ahahmed202025@gmail.com>
2021-11-21 00:55:14 +0200

    feat: add Map Component

--- app/components/map/Map.tsx
+++ app/components/map/Map.tsx
@@ -4,10 +4,19 @@ import {
   Marker,
   Polyline,
   Polygon,
+  InfoBox,
+  InfoWindow,
 } from "@react-google-maps/api";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import PolygonOptions from "../../constants/PolygonOptions";
 import PolyLineOptions from "../../constants/PolyLineOptions";
+import * as types from "../../types/Types";
+import * as utils from "../../utils/myUtils";
+import toast, { Toaster } from "react-hot-toast";
+import { useStore } from "../../store";
+import { getCenter } from "../../utils/myUtils";
+import useApi from "../../hooks/useApi";
+
 const containerStyle = {
   width: "100vw",
   height: "100vh",
@@ -17,12 +26,14 @@ const center = {
   lat: -3.745,
   lng: -38.523,
 };
-export type PolygonObject = {
-  path: Object;
-  options: Object;
-  name: String;
-};
+
 const Map = (props: any) => {
+  const { zones, token } = useStore();
+  null;
+
+  const [zoneId, setZoneId] = useState<String>("");
+  const [zone, setZone] = useState<types.ExportedZone>();
+
   const markerHandler = (sentMarker: Object, index: Number) => {
     if (index !== 0) {
       const finalMarkers = props.markers.filter(
@@ -30,54 +41,120 @@ const Map = (props: any) => {
       );
       props.setMarkers(finalMarkers);
     } else if (index === 0 && props.markers.length > 2) {
-      const polygonInstance = {
-        path: props.markers,
-        options: PolygonOptions("#000000"),
-        name: "",
+      const zoneInstance = {
+        points: props.markers,
+        color: "#000000",
+        label: "",
       };
-      props.setCurrentPolygon(polygonInstance);
-      props.openModal();
+      if (zones) {
+        const isIntersect = utils.checkIntersectionBetweenAll(
+          zoneInstance,
+          zones
+        );
+        if (isIntersect) {
+          toast.error("Overlapping is not possible");
+          props.setMarkers([]);
+        } else {
+          props.setCurrentZone(zoneInstance);
+          props.openAddModal();
+        }
+      }
     }
   };
-  const onMapLocationChange = useCallback((e: google.maps.MouseEvent) => {
-    const newLocation = e.latLng.toJSON();
+  const options: Object = { closeBoxURL: "", enableEventPropagation: true };
+
+  const onMapLocationChange = useCallback((e: google.maps.MapMouseEvent) => {
+    const newLocation = e.latLng?.toJSON();
     props.setMarkers((markers: []) => {
       return [...markers, newLocation];
     });
     console.log(newLocation);
   }, []);
 
+  const onZoneRightClick = (data: any, zone: types.ExportedZone) => {
+    const center = getCenter(zone.points);
+    props.setShowInfoWindow(center);
+    props.setZoneToBeEditedOrDeleted(zone);
+  };
+
+  const onDeleteHandler = () => {
+    props.openDeleteModal();
+    props.setShowInfoWindow(null);
+  };
+
+  const onEditHandler = () => {
+    props.openEditModal();
+    props.setShowInfoWindow(null);
+  };
+
   return (
-    <LoadScript googleMapsApiKey={`${process.env.NEXT_PUBLIC_GOOGLE_API}`}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onClick={onMapLocationChange}
-      >
-        {props.markers.map((marker: Object, index: any) => (
-          <Marker
-            key={index}
-            position={marker}
-            onClick={() => markerHandler(marker, index)}
-          />
-        ))}
-        <Polyline path={props.markers} options={PolyLineOptions()} />
+    <>
+      <div>
+        <Toaster />
+      </div>
 
-        {props.currentPolygon ? (
-          <Polygon
-            paths={props.currentPolygon.path}
-            options={props.currentPolygon.options}
-          />
-        ) : (
-          <></>
-        )}
+      <LoadScript googleMapsApiKey={`${process.env.NEXT_PUBLIC_GOOGLE_API}`}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onClick={onMapLocationChange}
+        >
+          {props.markers.map((marker: types.PathObject, index: any) => (
+            <Marker
+              key={index}
+              position={marker}
+              onClick={() => markerHandler(marker, index)}
+            />
+          ))}
+          <Polyline path={props.markers} options={PolyLineOptions()} />
 
-        {props.polygons.map((polygon: PolygonObject, index: any) => (
-          <Polygon key={index} paths={polygon.path} options={polygon.options} />
-        ))}
-      </GoogleMap>
-    </LoadScript>
+          {props.currentZone ? (
+            <Polygon
+              paths={props.currentZone.points}
+              options={PolygonOptions(props.currentZone.color)}
+            />
+          ) : (
+            <></>
+          )}
+
+          {zones.map((zone: types.ExportedZone, index: any) => {
+            return (
+              <>
+                <Polygon
+                  key={index}
+                  paths={zone.points}
+                  options={PolygonOptions(zone.color)}
+                  onRightClick={(rightClickReturn) =>
+                    onZoneRightClick(rightClickReturn, zone)
+                  }
+                />
+              </>
+            );
+          })}
+          {props.showInfoWindow && (
+            <InfoWindow options={options} position={props.showInfoWindow}>
+              <div className="flex flex-col">
+                <button
+                  onClick={() => {
+                    onEditHandler();
+                  }}
+                >
+                  Edit zone
+                </button>
+                <button
+                  onClick={() => {
+                    onDeleteHandler();
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      </LoadScript>
+    </>
   );
 };
 
