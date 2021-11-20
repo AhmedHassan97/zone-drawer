93eb6c1b34d27a5b7527697eed9f37c9be34ba6e
AhmedHassan97 <ahahmed202025@gmail.com>
2021-11-21 00:57:41 +0200

    feat: add my utils

---
+++ app/utils/myUtils.ts
@@ -0,0 +1,43 @@
+import * as types from "../types/Types";
+import intersect from "./utils";
+export const changeObjectPropertyNames = (zone: types.Zone) => {
+  const newPath: Array<Object> = [];
+  zone.points.map((point: types.PathObject) => {
+    newPath.push({ x: Number(point.lat), y: Number(point.lng) });
+  });
+  console.log(newPath);
+  return newPath;
+};
+export const getCenter = (points: Array<types.PathObject>) => {
+  let lat = 0;
+  let lng = 0;
+  points.map((point: types.PathObject) => {
+    lat += point.lat;
+    lng += point.lng;
+  });
+  const center: types.PathObject = {
+    lat: lat / points.length,
+    lng: lng / points.length,
+  };
+  return center;
+};
+export const checkIntersection = (zone1: types.Zone, zone2: types.Zone) => {
+  const intersection = intersect(
+    changeObjectPropertyNames(zone1),
+    changeObjectPropertyNames(zone2)
+  );
+  return intersection;
+};
+
+export const checkIntersectionBetweenAll = (
+  zone: types.Zone,
+  zones: Array<types.Zone>
+) => {
+  for (let index = 0; index < zones.length; index++) {
+    const intersection: any = checkIntersection(zones[index], zone);
+    if (intersection.length > 0) {
+      return true;
+    }
+  }
+  return false;
+};
