3e9a0b21729f5e625c4cc514b75cf4e527790bfc
AhmedHassan97 <ahahmed202025@gmail.com>
2021-11-21 14:21:04 +0200

    fix: fix meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,32 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Geo-Fencing</title>
+        <meta
+          name="description"
+          content="On this website you can draw zone on google maps. Also you can edit or delete the zones you created."
+        ></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta name="og:title" property="og:title" content="Geo-Fencing"></meta>
+        <meta
+          name="og:description"
+          property="og:description"
+          content="On this website you can draw zone on google maps. Also you can edit or delete the zones you created."
+        ></meta>
+        <meta property="og:site_name" content="Geo-Fencing"></meta>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
