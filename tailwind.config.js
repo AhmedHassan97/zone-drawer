388d3dc68036142d3806838759a1933ba56b766b
AhmedHassan97 <ahahmed202025@gmail.com>
2021-11-21 02:47:11 +0200

    fix: fix darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./app/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
   },
